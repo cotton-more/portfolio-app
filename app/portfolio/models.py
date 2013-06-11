f25895ffa051302b45c1a10fd0db54ca9bd8d02b
Ivan Nikulin <vansanblch@gmail.com>
2013-06-11 15:26:27 +0400

    collapse projects and cards into one table, add getter/setter for project.tags

--- app/portfolio/models.py
+++ app/portfolio/models.py
@@ -1,51 +1,64 @@
+from sqlalchemy.ext.hybrid import hybrid_property
+
 from app import db
 
-class Project(db.Model):
-    __tablename__ = 'projects'
+class Portfolio(db.Model):
+    __tablename__ = 'portfolio'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64))
     about = db.Column(db.Text)
-    tags = db.Column(db.String(255))
+    type = db.Column(db.String(20))
 
-    cards = db.relationship("Card",
-                          backref="menu"
-                          )
+    __mapper_args__ = {
+        'polymorphic_on': type
+    }
+
+
+class Project(Portfolio):
+    __mapper_args__ = {
+        'polymorphic_identity': 'project'
+    }
+
+    _tags = db.Column('tags', db.String(255))
+
+    @hybrid_property
+    def tags(self):
+        if self._tags is None:
+            return None
+        else:
+            return [ tag.strip() for tag in self._tags.split(',') ]
+
+    @tags.setter
+    def tags(self, value):
+        self._tags = value.join(',')
 
     def json(self):
         """Turn entity into json-ready object"""
 
-        project = {
+        return {
             'id': self.id,
             'name': self.name,
             'about': self.about,
-            'tags': self.tags,
-            'cards_len': len(self.cards)
+            'tags': self.tags
         }
-
-        if (project['tags']):
-            project['tags'] = [ tag.strip() for tag in self.tags.split(',') ]
-
-        return project
 
     def __repr__(self):
         return "<Project(%s, %s)>" % (self.id, self.name)
 
 
-class Card(db.Model):
-    __tablename__ = 'cards'
+class Card(Portfolio):
+    __mapper_args__ = {
+        'polymorphic_identity': 'card'
+    }
 
-    id = db.Column(db.Integer, primary_key=True)
-    project_id = db.Column(db.Integer, db.ForeignKey('projects.id'))
-    name = db.Column(db.String(128))
-    about = db.Column(db.Text)
+    parent_id = db.Column(db.Integer, db.ForeignKey('portfolio.parent_id'))
     image = db.Column(db.String(128))
 
     def json(self):
         """Turn entity into json-ready object"""
         return {
             'id': self.id,
-            'project_id': self.project_id,
             'name': self.name,
             'about': self.about,
             'image': self.image
--- app/portfolio/views.py
+++ app/portfolio/views.py
@@ -1,24 +1,38 @@
-from flask import Blueprint, jsonify, render_template
+import json
 
-from app.portfolio.models import Project, Card
+from flask import Blueprint
+from flask import Response
+from flask import render_template
+
+from app.portfolio.models import Portfolio
+from app.portfolio.models import Card
+from app.portfolio.models import Project
 
 mod = Blueprint('portfolio', __name__,
                 url_prefix='/portfolio',
                 template_folder='templates')
 
+
+@mod.route('/<int:id>/save', methods=["POST"])
+def save(id):
+    entity = Portfolio.query.get(id)
+
+    return Response(json.dumps(entity), mimetype='application/json')
+
+
 @mod.route('/projects')
 def menu_list():
     result = [e.json() for e in Project.query.all()]
 
-    return jsonify(result=result)
+    return Response(json.dumps(result), mimetype='application/json')
 
 
 @mod.route('/<int:project_id>/cards')
 def get_cards(project_id):
-    q = Card.query.filter(Card.project_id==project_id)
-    cards = [e.json() for e in q.all()]
+    q = Card.query.filter(Card.parent_id==project_id)
+    result = [e.json() for e in q.all()]
 
-    return jsonify(result=cards)
+    return Response(json.dumps(result), mimetype='application/json')
 
 
 @mod.route('/', defaults={'path': ''})
