a15d3700a797b3dc37b0062813f7cc7bb44b6c4b
Ivan Nikulin <vansanblch@gmail.com>
2013-07-10 17:30:33 +0400

    add login element and routes for logging in and logging out

--- app/users/views.py
+++ app/users/views.py
@@ -1,30 +1,15 @@
-from flask import Blueprint, jsonify, session, abort, request
+from flask import Blueprint, jsonify, session
 
 mod = Blueprint('users', __name__, url_prefix='/users')
 
-@mod.route('/login')
+@mod.route('/login', methods=['POST'])
 def auth_login():
-    assertion = request.args.get('assertion', False)
-    if False == assertion:
-        abort(400)
 
-    import requests
-    assertion_info = {'assertion': assertion,
-                        'audience': '127.0.0.1:5000' } # window.location.host
-    resp = requests.post('https://verifier.login.persona.org/verify',
-                        data=assertion_info, verify=True)
-
-    if not resp.ok:
-        abort(500)
-
-    data = resp.json()
-
-    if data['status'] == 'okay':
-        session.update({'email': data['email']})
+    data = {'email': 'test@example.com'}
 
     return jsonify(data)
 
-@mod.route('/logout')
+@mod.route('/logout', methods=['POST'])
 def auth_logout():
     session.pop('email', None)
     email = session.get('email', None)
