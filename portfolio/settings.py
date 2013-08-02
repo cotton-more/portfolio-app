206ebb049e28048a3ce9fad7e8f18e5fffe990ac
Ivan Nikulin <vansanblch@gmail.com>
2013-08-02 16:10:05 +0400

    fix path to database, add DEBUG to settings

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -1,5 +1,7 @@
 import os
-basedir = os.path.abspath('../' + os.path.dirname(__file__))
+basedir = os.path.abspath(os.path.dirname(__file__) + '/..')
+
+DEBUG = True
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 SQLALCHEMY_ECHO = True
