9c3646ea55031d7fa1b237266bebcc7c65d6faae
Ivan Nikulin <vansanblch@gmail.com>
2013-08-22 15:43:04 +0400

    add users service

--- portfolio/services.py
+++ portfolio/services.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 
 from .projects import ProjectService, CardService
+from .users import UsersService
 
 projects = ProjectService()
 cards = CardService()
+
+users = UsersService()
